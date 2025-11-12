6b723394a88692bcb5db113a30a28d0205cd7dc4
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-11-12 17:49:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame, sys
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 from button import Button
+from Charts import IdentityPart4
 
 pygame.init()
 
@@ -7,9 +11,16 @@ screen = pygame.display.set_mode((1680, 1050))
 pygame.display.set_caption("Menu")
 
 BackGround1 = pygame.image.load("/Users/evaldsberzins/pygame/RayRhythm/graphics/background2.jpg")
+LevelSelectBG = pygame.image.load("/Users/evaldsberzins/pygame/RayRhythm/graphics/level-select-background.png")
 
-# Music generator
-pygame.mixer_music.load("/Users/evaldsberzins/pygame/RayRhythm/sounds/main_menu_music.mp3")
+# Sound generator
+music_volume = 0.5
+sound_effect_volume = 0.5
+
+Menu_Song = pygame.mixer_music.load("/Users/evaldsberzins/pygame/RayRhythm/sounds/main_menu_music.mp3")
+Menu_Song = pygame.mixer_music.set_volume(music_volume)
+click_SFX = pygame.mixer.Sound("/Users/evaldsberzins/pygame/RayRhythm/sounds/click-sound.wav")
+click_SFX.set_volume(sound_effect_volume)
 
 def get_main_menu_font(size):
     return pygame.font.Font("/Users/evaldsberzins/pygame/RayRhythm/fonts/Blastge DEMO VERSION.ttf", size)
@@ -18,14 +29,20 @@ def play():
     while True:
         pygame.mixer_music.stop()
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
+        LEVEL_1_MOUSE_POS = pygame.mouse.get_pos()
 
-        screen.fill("black")
+        screen.blit(LevelSelectBG, (0, 0))
 
         PLAY_TEXT = get_main_menu_font(45).render("This is the PLAY screen.", True, "White")
-        PLAY_RECT = PLAY_TEXT.get_rect(center=(840, 360))
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(840, 280))
         screen.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(840, 560), text_input="BACK", font=get_main_menu_font(75), base_color="White", hovering_color="Green")
+        LEVEL_1 = Button(image=pygame.image.load("/Users/evaldsberzins/pygame/RayRhythm/graphics/level-1.png"), pos=(840, 500),
+                             text_input="Identity Part 4", font=get_main_menu_font(50), base_color="White", hovering_color="#B1D2EC")
+        LEVEL_1.changeColor(LEVEL_1_MOUSE_POS)
+        LEVEL_1.update(screen)
+
+        PLAY_BACK = Button(image=None, pos=(840, 800), text_input="BACK", font=get_main_menu_font(75), base_color="White", hovering_color="White")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(screen)
@@ -36,39 +53,43 @@ def play():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    click_SFX.play()
                     main_menu()
+                if LEVEL_1.checkForInput(PLAY_MOUSE_POS):
+                    IdentityPart4.start_identity_part4(screen)
         
         pygame.display.update()
 
 def options():
     while True:
+        events = pygame.event.get()
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        screen.fill("white")
-
-        OPTIONS_TEXT = get_main_menu_font(45).render("This is the OPTIONS screen.", True, "Black")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(840, 360))
-        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
-
-        OPTIONS_BACK = Button(image=None, pos=(840, 540), 
-                            text_input="BACK", font=get_main_menu_font(75), base_color="Black", hovering_color="Green")
-
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(screen)
-
-        for event in pygame.event.get():
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    click_SFX.play()
                     main_menu()
+
+        screen.fill("white")
+
+        OPTIONS_TEXT = get_main_menu_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(840, 150))
+        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None,pos=(840, 750),text_input="BACK",font=get_main_menu_font(75),base_color="Black",hovering_color="Green")
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(screen)
 
         pygame.display.update()
 
+
+
 def main_menu():
     pygame.mixer_music.play()
-    pygame.mixer_music.set_volume(0.2)
     while True:
         screen.blit(BackGround1, (0, 0))
 
@@ -95,18 +116,24 @@ def main_menu():
             button.changeColor(MENU_MOUSE_POS)
             button.update(screen)
         
-        for event in pygame.event.get():
+        events = pygame.event.get()
+
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    click_SFX.play()
                     play()
                 if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    click_SFX.play()
                     options()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
+
         
         pygame.display.update()
 
