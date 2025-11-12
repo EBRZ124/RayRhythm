47e0095e50bd6f6587dd541e78e64301fa8a8811
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-11-12 18:23:20 +0200

    Update IdentityPart4.py
    When a player presses a corresponding button, the playing cirles indicate that

--- Charts/IdentityPart4.py
+++ Charts/IdentityPart4.py
@@ -1,19 +1,20 @@
 import pygame, sys
 from button import Button
+import time
 
 pygame.init()
 pygame.mixer.init()
 
 IdentityBackground = pygame.image.load("/Users/evaldsberzins/pygame/RayRhythm/Charts/IdentityBG.png")
 GameplayOverlay = pygame.image.load("/Users/evaldsberzins/pygame/RayRhythm/graphics/gameplay-field.png")
+PressedCircle = pygame.image.load("/Users/evaldsberzins/pygame/RayRhythm/graphics/circle_pressed.png")
 
 def start_identity_part4(screen):
-    pygame.mixer.music.load("/Users/evaldsberzins/pygame/RayRhythm/Charts/Identity-Part-4.mp3")
+    pygame.mixer.music.load("/Users/evaldsberzins/pygame/RayRhythm/Charts/Identity-Part-4.wav")
     pygame.mixer.music.set_volume(0.2)
     pygame.mixer_music.play()
 
     running = True
-    player = pygame.Rect(100, 100, 50, 50)
 
     while running:
         for event in pygame.event.get():
@@ -24,13 +25,17 @@ def start_identity_part4(screen):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
             running = False
-        if keys[pygame.K_RIGHT]:
-            player.x += 5
-        if keys[pygame.K_LEFT]:
-            player.x -= 5
-
         screen.blit(IdentityBackground, (0, 0))
         screen.blit(GameplayOverlay, (0, 0))
-        pygame.draw.rect(screen, "red", player)
+        
+        if keys[pygame.K_d]:
+            screen.blit(PressedCircle, (903.5, 882.2))
+        if keys[pygame.K_f]:
+            screen.blit(PressedCircle, (1079.2, 882.2))
+        if keys[pygame.K_j]:
+            screen.blit(PressedCircle, (1251.6, 882.2))
+        if keys[pygame.K_k]:
+            screen.blit(PressedCircle, (1424, 882.2))
+
 
         pygame.display.flip()
