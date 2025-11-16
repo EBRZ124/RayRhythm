c1b77c02932b659d9928732d2352cc7331218e64
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-11-16 16:23:20 +0200

    Seperate test level chart file

---
+++ Charts/TestLevel/TestLevel_chart.py
@@ -0,0 +1,44 @@
+# Mapped ntoes for the test level
+
+note_delay = 0
+
+TestLevel_notes = [
+    {"time": 900+note_delay, "lane": 0}, #1
+    {"time": 1328+note_delay, "lane": 1}, #2
+    {"time": 1756+note_delay, "lane": 2}, #3
+    {"time": 2148+note_delay, "lane": 3}, #4
+    {"time": 2148+note_delay, "lane": 2}, #4
+    {"time": 2576+note_delay, "lane": 2}, #5
+    {"time": 3004+note_delay, "lane": 3}, #6
+    {"time": 3004+note_delay, "lane": 2}, #6
+    {"time": 3432+note_delay, "lane": 2}, #7
+    {"time": 3860+note_delay, "lane": 3}, #8
+    {"time": 3860+note_delay, "lane": 2}, #8
+    {"time": 4288+note_delay, "lane": 0}, #9
+    {"time": 4716+note_delay, "lane": 2}, #10
+    {"time": 5144+note_delay, "lane": 1}, #11
+    {"time": 5527+note_delay, "lane": 2}, #12
+    {"time": 6000+note_delay, "lane": 2}, #13
+    {"time": 6428+note_delay, "lane": 1}, #14
+    {"time": 6856+note_delay, "lane": 3}, #15
+    {"time": 7284+note_delay, "lane": 1}, #16
+    {"time": 7712+note_delay, "lane": 3}, #17
+    {"time": 8140+note_delay, "lane": 2}, #18
+    {"time": 8568+note_delay, "lane": 1}, #19
+    {"time": 8996+note_delay, "lane": 0}, #20
+    {"time": 9424+note_delay, "lane": 0}, #21
+    {"time": 9852+note_delay, "lane": 1}, #22
+    {"time": 10280+note_delay, "lane": 0}, #23
+    {"time": 10708+note_delay, "lane": 0}, #24
+    {"time": 11136+note_delay, "lane": 2}, #25
+    {"time": 11564+note_delay, "lane": 0}, #26
+    {"time": 11992+note_delay, "lane": 0}, #27
+    {"time": 12420+note_delay, "lane": 3}, #28
+    {"time": 12848+note_delay, "lane": 2}, #29
+    {"time": 13276+note_delay, "lane": 3}, #30
+    {"time": 13276+note_delay, "lane": 2}, #30
+    {"time": 13704+note_delay, "lane": 3}, #31
+    {"time": 13704+note_delay, "lane": 2}, #31
+    {"time": 14132+note_delay, "lane": 3}, #32
+    {"time": 14132+note_delay, "lane": 2}, #32
+]
