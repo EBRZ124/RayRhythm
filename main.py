c65d6fc6fa80d7cf193fe078a56704483f5a28d3
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-12-03 18:05:34 +0200

    Asset change
    Assets are all loaded in a seperate global_variables file

--- main.py
+++ main.py
@@ -5,35 +5,29 @@ from pygame_widgets.textbox import TextBox
 from pygame_widgets.slider import Slider
 from Charts.TestLevel import TestLevel
 from Charts.Snowy import snowy
+import global_variables
 
 pygame.init()
+screen_scaler = global_variables.global_scaler
+screen = pygame.display.set_mode((1680*screen_scaler, 1050*screen_scaler))
+pygame.display.set_caption("RayRhythm")
 
-screen = pygame.display.set_mode((1680, 1050))
-pygame.display.set_caption("Menu")
+BackGround1 = global_variables.main_menu_bg
+LevelSelectBG = global_variables.level_select_bg
+OptionsMenuBG = global_variables.options_menu_bg
 
-BackGround1 = pygame.image.load("RayRhythm/graphics/background2.jpg")
-LevelSelectBG = pygame.image.load("RayRhythm/graphics/level-select-background.png")
-OptionsMenuBG = pygame.image.load("RayRhythm/graphics/options-menu-bg.png")
-
-# Sound generator
 music_volume = 0.5
 sound_effect_volume = 0.5
 
-pygame.mixer_music.load("RayRhythm/sounds/main_menu_music.mp3")
+pygame.mixer_music.load("/Users/evaldsberzins/pygame/RayRhythm/sounds/main_menu_music.mp3")
 pygame.mixer_music.set_volume(music_volume)
-click_SFX = pygame.mixer.Sound("RayRhythm/sounds/click-sound.wav")
+click_SFX = pygame.mixer.Sound("/Users/evaldsberzins/pygame/RayRhythm/sounds/click-sound.wav")
 click_SFX.set_volume(sound_effect_volume)
 
 selected_skin = 0
 
-def get_main_menu_font(size):
-    return pygame.font.Font("RayRhythm/fonts/Blastge DEMO VERSION.ttf", size)
-
-def get_level_name_font(size):
-    return pygame.font.Font("RayRhythm/fonts/capitolcity.ttf", size)
-
 def main_menu():
-    pygame.mixer_music.load("RayRhythm/sounds/main_menu_music.mp3")
+    pygame.mixer_music.load("/Users/evaldsberzins/pygame/RayRhythm/sounds/main_menu_music.mp3")
     pygame.mixer_music.set_volume(music_volume)
     pygame.mixer_music.play()
     while True:
@@ -41,19 +35,19 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_main_menu_font(100).render("RayRhythm", True, "White")
-        MENU_RECT = MENU_TEXT.get_rect(center=(840, 200))
-        TEXT_BG = pygame.image.load("RayRhythm/graphics/Title-Rect.png").convert_alpha()
-        TEXT_BG = pygame.transform.scale(TEXT_BG, (MENU_RECT.width + 200, MENU_RECT.height + 100))
+        MENU_TEXT = global_variables.get_main_menu_font(int(100*screen_scaler)).render("RayRhythm", True, "White")
+        MENU_RECT = MENU_TEXT.get_rect(center=(840*screen_scaler, 200*screen_scaler))
+        TEXT_BG = global_variables.tite_text_box
+        TEXT_BG = pygame.transform.scale(TEXT_BG, ((MENU_RECT.width + 200*screen_scaler), (MENU_RECT.height + 100*screen_scaler)))
         BG_RECT = TEXT_BG.get_rect(center=MENU_RECT.center)
         BG_RECT.y -= 10
 
-        PLAY_BUTTON = Button(image=pygame.image.load("RayRhythm/graphics/Play-Rect.png"), pos=(840, 400),
-                             text_input="PLAY", font=get_main_menu_font(75), base_color="#E57B1E", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("RayRhythm/graphics/Options-Rect.png"), pos=(840, 550),
-                             text_input="OPTIONS", font=get_main_menu_font(75), base_color="#E57B1E", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("RayRhythm/graphics/Quit-Rect.png"), pos=(840, 700),
-                             text_input="QUIT", font=get_main_menu_font(75), base_color="#E57B1E", hovering_color="White")
+        PLAY_BUTTON = Button(image=global_variables.play_button_box, pos=(840*screen_scaler, 400*screen_scaler),
+                             text_input="PLAY", font=global_variables.get_main_menu_font(int(75*screen_scaler)), base_color="#E57B1E", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=global_variables.options_button_box, pos=(840*screen_scaler, 550*screen_scaler),
+                             text_input="OPTIONS", font=global_variables.get_main_menu_font(int(75*screen_scaler)), base_color="#E57B1E", hovering_color="White")
+        QUIT_BUTTON = Button(image=global_variables.quit_button_box, pos=(840*screen_scaler, 700*screen_scaler),
+                             text_input="QUIT", font=global_variables.get_main_menu_font(int(75*screen_scaler)), base_color="#E57B1E", hovering_color="White")
         
         screen.blit(TEXT_BG, BG_RECT)
         screen.blit(MENU_TEXT, MENU_RECT)
@@ -79,13 +73,12 @@ def main_menu():
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
-
         
         pygame.display.update()
     
 def play():
     pygame.mixer_music.stop()
-    pygame.mixer_music.load("RayRhythm/sounds/level_select_music.mp3")
+    pygame.mixer_music.load("/Users/evaldsberzins/pygame/RayRhythm/sounds/level_select_music.mp3")
     music_volume = 0.3
     pygame.mixer_music.set_volume(music_volume)
     pygame.mixer_music.play()
@@ -95,22 +88,22 @@ def play():
 
         screen.blit(LevelSelectBG, (0, 0))
         
-        LEVEL_SNOWY = Button(image=pygame.image.load("RayRhythm/graphics/level-2.png"), pos=(840, 200),
-                             text_input="Snowy", font=get_level_name_font(50), base_color="White", hovering_color="#D3DDF8")
+        LEVEL_SNOWY = Button(image=global_variables.snowy_box, pos=(840*screen_scaler, 200*screen_scaler),
+                            text_input="Snowy", font=global_variables.get_level_name_font(int(60*screen_scaler)), base_color="White", hovering_color="#D3DDF8")
         LEVEL_SNOWY.changeColor(LEVEL_MOUSE_POS)
         LEVEL_SNOWY.update(screen)
 
-        TEST_LEVEL = Button(image=pygame.image.load("RayRhythm/graphics/test-level.png"), pos=(840, 700),
-                             text_input="TEST LEVEL", font=get_level_name_font(50), base_color="White", hovering_color="#FFFBDA")
+        TEST_LEVEL = Button(image=global_variables.test_level_box, pos=(840*screen_scaler, 700*screen_scaler),
+                            text_input="TEST LEVEL", font=global_variables.get_level_name_font(int(60*screen_scaler)), base_color="White", hovering_color="#FFFBDA")
         TEST_LEVEL.changeColor(LEVEL_MOUSE_POS)
         TEST_LEVEL.update(screen)
 
-        LEVEL_ANEMONE = Button(image=pygame.image.load("RayRhythm/graphics/level-1.png"), pos=(840, 450),
-                             text_input="ANEMONE", font=get_level_name_font(50), base_color="White", hovering_color="#C2F1FF")
+        LEVEL_ANEMONE = Button(image=global_variables.anemone_box, pos=(840*screen_scaler, 450*screen_scaler),
+                              text_input="ANEMONE", font=global_variables.get_level_name_font(int(60*screen_scaler)), base_color="White", hovering_color="#C2F1FF")
         LEVEL_ANEMONE.changeColor(LEVEL_MOUSE_POS)
         LEVEL_ANEMONE.update(screen)
 
-        PLAY_BACK = Button(image=None, pos=(840, 950), text_input="BACK", font=get_main_menu_font(75), base_color="White", hovering_color="#F4CCFC")
+        PLAY_BACK = Button(image=None, pos=(840*screen_scaler, 950*screen_scaler), text_input="BACK", font=global_variables.get_main_menu_font(int(75*screen_scaler)), base_color="White", hovering_color="#F4CCFC")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(screen)
@@ -126,10 +119,10 @@ def play():
                 if LEVEL_ANEMONE.checkForInput(PLAY_MOUSE_POS):
                     anamone.start_anamone(screen, selected_skin)
                 if TEST_LEVEL.checkForInput(PLAY_MOUSE_POS):
-                    TestLevel.start_test_level(screen, selected_skin)
+                    TestLevel.start_test_level(screen, selected_skin, screen_scaler)
                 if LEVEL_SNOWY.checkForInput(PLAY_MOUSE_POS):
                     snowy.start_snowy(screen, selected_skin)
-        
+  
         pygame.display.update()
 
 def options():
@@ -155,33 +148,33 @@ def options():
 
         screen.blit(OptionsMenuBG, (0, 0))
 
-        OPTIONS_TEXT = get_main_menu_font(75).render("Settings Menu", True, "White")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(840, 100))
+        OPTIONS_TEXT = global_variables.get_main_menu_font(int(75*screen_scaler)).render("Settings Menu", True, "White")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(840*screen_scaler, 100*screen_scaler))
         screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        SKINS_TEXT = get_main_menu_font(60).render("SKINS", True, "White")
-        SKINS_RECT = SKINS_TEXT.get_rect(center=(840, 660))
+        SKINS_TEXT = global_variables.get_main_menu_font(int(60*screen_scaler)).render("SKINS", True, "White")
+        SKINS_RECT = SKINS_TEXT.get_rect(center=(840*screen_scaler, 640*screen_scaler))
         screen.blit(SKINS_TEXT, SKINS_RECT)
 
         if selected_skin == 0:
-            SKIN_CIRCLE = Button(image=pygame.image.load("RayRhythm/graphics/circle-skin-selected.png"), pos=(670, 775), text_input=" ", font=get_level_name_font(75), base_color="White", hovering_color="White")
+            SKIN_CIRCLE = Button(image=global_variables.circle_skin_selected, pos=(670*screen_scaler, 775*screen_scaler), text_input=" ", font=global_variables.get_level_name_font(75), base_color="White", hovering_color="White")
             SKIN_CIRCLE.changeColor(OPTIONS_MOUSE_POS)
             SKIN_CIRCLE.update(screen)
 
-            SKIN_RAYMAN = Button(image=pygame.image.load("RayRhythm/graphics/rayman-skin-button.png"), pos=(1010, 775), text_input=" ", font=get_level_name_font(75), base_color="White", hovering_color="White")
+            SKIN_RAYMAN = Button(image=global_variables.rayman_skin_unselected, pos=(1010*screen_scaler, 775*screen_scaler), text_input=" ", font=global_variables.get_level_name_font(75), base_color="White", hovering_color="White")
             SKIN_RAYMAN.changeColor(OPTIONS_MOUSE_POS)
             SKIN_RAYMAN.update(screen)
 
         if selected_skin == 1:
-            SKIN_CIRCLE = Button(image=pygame.image.load("RayRhythm/graphics/circle-skin-button.png"), pos=(670, 775), text_input=" ", font=get_level_name_font(75), base_color="White", hovering_color="White")
+            SKIN_CIRCLE = Button(image=global_variables.circle_skin_unselected, pos=(670*screen_scaler, 775*screen_scaler), text_input=" ", font=global_variables.get_level_name_font(75), base_color="White", hovering_color="White")
             SKIN_CIRCLE.changeColor(OPTIONS_MOUSE_POS)
             SKIN_CIRCLE.update(screen)
 
-            SKIN_RAYMAN = Button(image=pygame.image.load("RayRhythm/graphics/rayman-skin-selected.png"), pos=(1010, 775), text_input=" ", font=get_level_name_font(75), base_color="White", hovering_color="White")
+            SKIN_RAYMAN = Button(image=global_variables.rayman_skin_selected, pos=(1010*screen_scaler, 775*screen_scaler), text_input=" ", font=global_variables.get_level_name_font(75), base_color="White", hovering_color="White")
             SKIN_RAYMAN.changeColor(OPTIONS_MOUSE_POS)
             SKIN_RAYMAN.update(screen)  
 
-        OPTIONS_BACK = Button(image=None,pos=(840, 950),text_input="BACK",font=get_main_menu_font(75),base_color="White",hovering_color="#F4CCFC")
+        OPTIONS_BACK = Button(image=None,pos=(840*screen_scaler, 950*screen_scaler),text_input="BACK",font=global_variables.get_main_menu_font(int(75*screen_scaler)),base_color="White",hovering_color="#F4CCFC")
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(screen)
 
